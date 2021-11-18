99b72e39af9389bacbb5df85f3a227c282cf423b
MaxS <Max270899@gmail.com>
2021-11-18 16:45:44 +0100

    méthode pour créer l'utilisateur

--- controlers/userControler.js
+++ controlers/userControler.js
@@ -4,5 +4,12 @@ module.exports = {
     getAllUsers: async(req,res)=>{
         const users = await userModel.find();
         res.status(200).json({users})
+    },
+    postUser: async(req,res)=>{
+        const user = await userModel.create({
+            email: 'Max@gmail.com',
+            mdp: 'test8888', 
+        });
+        res.status(201).json({user})
     }
 }
