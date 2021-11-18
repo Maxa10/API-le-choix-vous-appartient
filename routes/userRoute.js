ea43feb8aad8b57d8efc844827621b7ff5ec36ac
MaxS <Max270899@gmail.com>
2021-11-18 16:00:07 +0100

    changement de chemin

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,5 @@
 const express = require("express");
 const router = express.Router();
 const userControler = require("../controlers/userControler");
-router.get("/api/users", userControler.getAllUsers);
+router.get("/", userControler.getAllUsers);
 module.exports = router
