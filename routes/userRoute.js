d0029408053dc8a71c3a89eb9977b00b81d43ba8
MaxS <Max270899@gmail.com>
2021-11-18 16:46:24 +0100

    créer une nouvelle route post

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,5 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const userControler = require("../controlers/userControler");
+
 router.get("/", userControler.getAllUsers);
+
+router.post("/", userControler.postUser);
+
 module.exports = router
