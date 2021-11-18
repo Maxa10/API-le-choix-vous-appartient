325ec3a7d59f2bdf4eb64394deb6013af391a36d
MaxS <Max270899@gmail.com>
2021-11-18 15:44:44 +0100

    test routes

--- serveur.js
+++ serveur.js
@@ -3,11 +3,18 @@ const express = require("express");
 const app = express();
 const port = process.env.APP || 3500;
 const connexion = require("./database");
-console.log(connexion);
 console.log(port);
 app.listen(port, function(err){
     if (err){
         console.log(err);
     }
     console.log("server listening on port" + port);
+});
+
+app.get("/",(req,res)=>{ 
+    res.send("ok ça fonctionne")
+});
+
+app.get("/test",(req,res)=>{
+    res.send("test")
 })
