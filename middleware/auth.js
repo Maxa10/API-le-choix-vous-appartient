acf047f66c3ca0f806c63ca1aa74ac1d631b7fdd
MaxS <Max270899@gmail.com>
2021-11-19 16:59:26 +0100

    création de la verification du token et du decodage

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,1 +1,17 @@
+require("dotenv").config();
 const jwt = require("jsonwebtoken");
+const verifyToken = (req,res,next)=>{
+    const token = req.headers["x-access-token"]
+    if (!token){
+        return res.status(403).json({msg:"No token provided"})
+    }
+    try{
+        const decoded = jwt.verify(token, process.env.PRIVATE_KEY)
+        req.user = decoded
+    } 
+    catch (err){
+       return res.status(420).json({msg:"erreur pendant la verification du token"})
+    }
+    return next()
+}
+module.exports = verifyToken
