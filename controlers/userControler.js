900a2fd505c9120d4aafc6d0b233dab207209015
MaxS <Max270899@gmail.com>
2021-11-19 16:00:46 +0100

    création d'un login mail mdp token

--- controlers/userControler.js
+++ controlers/userControler.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const userModel = require("../models/userModel");
 
 module.exports = {
@@ -11,5 +12,30 @@ module.exports = {
             mdp: 'test8888', 
         });
         res.status(201).json({user})
+    },
+    login: async(req,res)=>{
+        try{
+            const {email, mdp} = req.body
+            if (!email || !mdp){
+                return res.status(204).json({msg:"Tout les champs ne sont pas remplis"});
+            }
+            const user = await userModel.findOne({email});
+            if (user && (await bcrypt.compare(mdp, user.mdp))){
+                const token = jwt.sign(
+                    {user_id: user._id},
+                    process.env.PRIVATE_KEY,
+                    {expiresIn:"1h"}
+                )
+                user.token = token;
+                res.status(200).json({user})
+            }
+            else {
+                res.status(400).json({msg:"Votre email ou votre mot de passe n'est pas correct"})
+            }
+        }
+        catch(err){
+            console.log(err);
+        }
     }
+
 }
