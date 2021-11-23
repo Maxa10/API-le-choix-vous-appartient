e4f957892b49e503ce51dbd3d02153f60b467c0e
Maxa10 <Max270899@gmail.com>
2021-11-23 11:39:27 +0100

    front

---
+++ front/script.js
@@ -0,0 +1,73 @@
+window.onload = function () {
+	const connexion = document.getElementById('connexion');
+	const inscription = document.getElementById('inscription');
+	const read = document.getElementById('read');
+
+	if (connexion) {
+		connexion.addEventListener('submit', (e) => {
+			e.preventDefault();
+			var datas = {
+				email: document.getElementById('emailCo').value,
+				password: document.getElementById('passwordCo').value,
+			};
+			fetch('http://localhost:3000/api/login/', {
+				method: 'POST',
+				mode: 'cors',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+				},
+				body: JSON.stringify(datas),
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					console.log(data);
+                    localStorage.setItem('user', data.user._id);
+                    window.location.href = "./read.html";
+				})
+				.catch((error) => console.error(error));
+		});
+	}
+
+	if (inscription) {
+		inscription.addEventListener('submit', (e) => {
+			e.preventDefault();
+			var datas = {
+				email: document.getElementById('emailIn').value,
+				name: document.getElementById('name').value,
+				password: document.getElementById('passwordIn').value,
+			};
+			console.log(datas);
+			fetch('http://localhost:3000/api/register/', {
+				method: 'POST',
+				mode: 'cors',
+				headers: {
+					'Content-Type': 'application/json',
+					Accept: 'application/json',
+				},
+				body: JSON.stringify(datas),
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					console.log(data);
+				})
+				.catch((error) => console.error(error));
+		});
+	}
+	if (read) {
+        const id = localStorage.getItem('user');
+		fetch('http://localhost:3000/api/users/' + id, {
+			method: 'GET',
+			mode: 'cors',
+			headers: {
+				'Content-Type': 'application/json',
+				Accept: 'application/json',
+			}
+		})
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+			})
+			.catch((error) => console.error(error));
+	}
+};
